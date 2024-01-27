a21f8db6ff28c3e5ca0e65ff36ca3400378f1296
Fahim Ahamed <121462098+TheBwof@users.noreply.github.com>
2024-01-27 13:59:54 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-   botToken: "6780485935:AAH0v8fYVPvTyS5Ed6yg_fEtTSRzSsKgp0I", // Modify your bot TOKEN here
-   hostURL: "localhost:3000", // Modify your host URL here
+   botToken: "YOUR_BOT_TOKEN", // Modify your bot TOKEN here
+   hostURL: "YOUR_URL", // Modify your host URL here
 };
 // for port forewording use "ssh -R 80:localhost:5000 serveo.net"
